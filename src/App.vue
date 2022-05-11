4025c79d8f3adc11b04bbea4c14f6b85dee2fe24
Marcelo Long <zacelo1977@hotmail.com>
2022-05-11 15:38:23 -0300

    listado con componente de Vue Cli

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <div class="container mt-3 text-center">
+      <div class="row">
+        <div class="col-8">
+          <ListaPersonas
+            :propsEncabezado="encabezado"
+            :propsListado="listado"
+            titulo="Listado General"
+          />
+          <br />
+          <ListaPersonas
+            :propsEncabezado="encabezado"
+            :propsListado=" filtrar('Femenino')"
+            titulo="Listado Mujeres"
+          />
+          <br />
+          <ListaPersonas
+            :propsEncabezado="encabezado"
+            :propsListado="filtrar('Masculino')"
+            titulo="Listado Hombres"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ListaPersonas from "./components/ListaPersonas.vue";
+
+export default {
+  name: "App",
+  components: {
+    ListaPersonas,
+  },
+  data() {
+    return {
+      encabezado: ["id", "Nombre", "Edad", "Sexo"],
+      listado: [
+        {
+          id: 1,
+          nombre: "Marcelo",
+          edad: 44,
+          sexo: "Masculino",
+        },
+        {
+          id: 2,
+          nombre: "Carla",
+          edad: 32,
+          sexo: "Femenino",
+        },
+        {
+          id: 3,
+          nombre: "Juan",
+          edad: 23,
+          sexo: "Masculino",
+        },
+        {
+          id: 4,
+          nombre: "María",
+          edad: 28,
+          sexo: "Femenino",
+        },
+        {
+          id: 5,
+          nombre: "Sandra",
+          edad: 46,
+          sexo: "Femenino",
+        },
+        {
+          id: 6,
+          nombre: "Carlos",
+          edad: 46,
+          sexo: "Masculino",
+        },
+      ],
+    };
+  },
+  methods: {
+    filtrar(genero) {
+      let listadoConFiltro = [];
+      for (let i of this.listado) {
+        if (i.sexo == genero) {
+          listadoConFiltro.push(i);
+        }
+      }
+      return listadoConFiltro;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ListaPersonas.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+     <h1>{{titulo}}</h1>
+    <table class="table">
+      <thead class="bg-warning">
+        
+        <tr>
+          <th scope="col" v-for="item in propsEncabezado" :key="item.id">
+            {{ item }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="itemList in propsListado"
+          :key="itemList.id"
+          :class="itemList.sexo"         
+        >   
+          <td>{{ itemList.id }}</td>
+          <td>{{ itemList.nombre }}</td>
+          <td>{{ itemList.edad }}</td>
+          <td>{{ itemList.sexo }}</td>    
+          
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListaPersonas",
+  props: {
+    propsEncabezado: {
+      type: Array,
+    },
+    propsListado: {
+      type: Array,
+    },
+    titulo: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.Femenino {
+  background-color: rgba(240, 190, 245, 0.363);
+}
+.Masculino {
+  background-color: rgb(166, 204, 255);
+}
+</style>
